417aefedc375b453b173644061bd90e81899cd8d
Kajal Singh <ks9392656@gmail.com>
2026-05-11 21:52:36 +0530

    made changes in app.py

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
 def main():
-    st.header("This is title")
+    if 'login_type' not in st.session_state:
+        st.session_state['login_type'] = None
+    
+    match st.session_state['login_stete']:
+        case 'teacher':
+            teacher_screen()
+        
+        case 'student':
+            student_screen()
+        
+        case None:
+            home_screen()
 main()
