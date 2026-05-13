b5c933773483d6c3addad794af63b8be0e98ff6f
Kajal Singh <ks9392656@gmail.com>
2026-05-13 22:12:22 +0530

    added changes in app

---
+++ src/ui/base_layout.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+def style_background_home():
+    
+    st.markdown(f"""
+        <style>
+            .stApp{
+                background: #5865F2 !important;
+            }
+        </style>     
+    """,unsafe_allow_html=True)
+
+def style_base_dashboard():
+
+    st.markdown(f"""
+        <style>
+            .stApp{
+                background: #5865F2 !important;
+            }
+        </style>     
+    """,unsafe_allow_html=True)
+
+def style_base_layout():
+
+    st.markdown("""
+        <style>
+        
+        </style>
+"""
+    )
